6bacb7a47b9f2c174997baff5efda47732ecc58b
Connor Detviler <connor.detviler@gmail.com>
2021-01-08 19:39:27 -0600

    created function displayList that displays todos on page. currently using client-side dummy data

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,42 @@
+const dummyData = [
+    {
+        text: 'Take garbage out',
+        completed: true
+    },
+    {
+        text: 'Work on weekend coding project',
+        completed: false
+    },
+    {
+        text: 'Practice piano',
+        completed: false
+    },
+    {
+        text: 'Eat junk food and pass out',
+        completed: true
+    }
+];
+
 console.log('client.js loaded');
 
 $(document).ready(function() {
     console.log('jQuery loaded');
+
+    $('#submit-btn').on('click', addTodo);
+
+    displayList(dummyData);
 });
+
+function addTodo() {
+    
+}
+
+
+function displayList(listData) {
+    $('#todo-list').empty();
+
+    for (let i = 0; i < listData.length; i++) {
+        //'completed-true', 'completed-false' classes allow styling to show whether a todo is completed
+        $('#todo-list').append(`<li class="completed-${listData[i].completed}">${listData[i].text}</li>`)
+    }
+}
