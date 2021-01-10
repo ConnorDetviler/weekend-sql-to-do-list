aa1e2a32f8c2f318af52278d5bb710d6fd2dfc9a
Connor Detviler <connor.detviler@gmail.com>
2021-01-10 16:48:37 -0600

    made changes to connect to database on heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -4,16 +4,28 @@ const pg = require('pg');
 const Pool = pg.Pool;
 const pool = new Pool ({
     //basically creating new connection with our database. 
-    database: 'todo_list', //the name of database, This can change!
-    host: 'localhost', // where is your database?
-    port: 5432, //the port for your database
+    // database: 'todo_list', //the name of database, This can change!
+    // host: 'localhost', // where is your database?
+    // port: 5432, //the port for your database
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
 })
 
 // helpers breadcrumbs
 pool.on('connect', ()=> {
     console.log('PG CONNECTED');
 })
-
+/////////// for heroku
+client.query('SELECT public,todos FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });
+/////////// ^ for heroku
 pool.on('error', (error)=>{
     console.log(error);
 })
